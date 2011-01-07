339bd4d110ae7991692e94bce51c590a13f8d992
tamoot <tamoot@gmail.com>
2011-01-08 03:10:06 +0900

    modify comment

--- itunes_app_quote.rb
+++ itunes_app_quote.rb
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
 #
-# itunes_app_quote.rb - embeded App info from itune url.
+# itunes_app_quote.rb - Making link with image to iTunes.
+#
+# see label: #{@lang}/itunes_app_quote.rb
 #
 # Copyright (C) 2011, tamoot <tamoot+tdiary@gmail.com>
 # You can redistribute it and/or modify it under GPL2.
